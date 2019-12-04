b6c0bc6e6bd5a476c079396588f321d7b1a29266
Jasper Bernales <bernalesjasper@gmail.com>
2019-12-04 18:45:08 +0800

    update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,6 @@ module.exports = {
     script: './build/server.js',
     instances: 1,
     watch: ['build'],
-    ignore_watch: ['node_modules', 'mnt']
+    ignore_watch: ['node_modules', 'mnt', 'tmp']
   }]
 }
