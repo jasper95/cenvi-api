88a0b0f321c149c4ad360499c9508ee75c1577df
Jasper Bernales <bernalesjasper@gmail.com>
2020-02-12 10:27:12 +0800

    useragent func

--- src/app/meta/controller.js
+++ src/app/meta/controller.js
@@ -13,27 +13,27 @@ export default class MetaTagsController {
     if (!singular_types.includes(type)) {
       type = `${type}s`
     }
-    if (!userAgent.test(/bot|crawler|spider|crawling/i)) {
-      res.redirect(301, [process.env.PORTAL_LINK, type, slug], next);
+    if (userAgent().test(/bot|crawler|spider|crawling/i)) {
+      const body = `
+        <html>
+          <head>
+            <meta property="og:type" content="article" />
+            <meta property="og:url": content="${['https://cenvi-api.herokuapp.com', 'meta', slug].join('/')}"/>
+            <meta property="og:title" content="${post.name}" />
+            <meta property="og:description" content="${post.excerpt}" />
+            <meta property="og:image" content="${[process.env.STATIC_URL, post.image_url].join('/')}" />
+          </head>
+          <body></body>
+        </html>
+      `
+      res.writeHead(200, {
+        'Content-Length': Buffer.byteLength(body),
+        'Content-Type': 'text/html'
+      });
+      res.write(body);
+      res.end();
       return
     }
-    const body = `
-      <html>
-        <head>
-          <meta property="og:type" content="article" />
-          <meta property="og:url": content="${['https://cenvi-api.herokuapp.com', 'meta', slug].join('/')}"/>
-          <meta property="og:title" content="${post.name}" />
-          <meta property="og:description" content="${post.excerpt}" />
-          <meta property="og:image" content="${[process.env.STATIC_URL, post.image_url].join('/')}" />
-        </head>
-        <body></body>
-      </html>
-    `
-    res.writeHead(200, {
-      'Content-Length': Buffer.byteLength(body),
-      'Content-Type': 'text/html'
-    });
-    res.write(body);
-    res.end();
+    res.redirect(301, [process.env.PORTAL_LINK, type, slug], next);
   }
 }
