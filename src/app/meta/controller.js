61977affb22bed582e699bc5f615a2b5d126e1d3
Jasper Bernales <bernalesjasper@gmail.com>
2020-02-12 10:20:01 +0800

    redirect if not bot

--- src/app/meta/controller.js
+++ src/app/meta/controller.js
@@ -5,21 +5,24 @@ export default class MetaTagsController {
     this.Model = Model
   }
 
-  async getMetaTags({ params, userAgent }, res) {
-    console.log('userAgent: ', userAgent);
-    const { slug } = params
-    const post = await this.DB.find('post', slug, [], 'slug')
+  async getMetaTags({ params, userAgent }, res, next) {
     const singular_types = ['news']
     let { type } = post
     if (!singular_types.includes(type)) {
       type = `${type}s`
     }
+    if (!userAgent.test(/bot|crawler|spider|crawling/i)) {
+      res.redirect(301, [process.env.PORTAL_LINK, type, slug], next);
+      return
+    }
+    const { slug } = params
+    const post = await this.DB.find('post', slug, [], 'slug')
     const body = `
       <html>
         <head>
           <meta property="og:type" content="article" />
           <meta property="og:url": content="${['https://cenvi-api.herokuapp.com', 'meta', slug].join('/')}"/>
-          <meta property="og:title" content="${post.title}" />
+          <meta property="og:title" content="${post.name}" />
           <meta property="og:description" content="${post.excerpt}" />
           <meta property="og:image" content="${[process.env.STATIC_URL, post.image_url].join('/')}" />
         </head>
